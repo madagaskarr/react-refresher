461c6034bcd5a16972578605cdb3466aa2e16071
Tigran <me@tigranes.io>
2021-01-31 16:08:28 -0800

    Added state to component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
 import ParagraphListComponent from "./components/ParagraphList/ParagraphListComponent";
 import SubmitComponent from "./components/SubmitComponent/SubmitComponent"
-import React from "react";
+import React, {useState} from "react";
+
 
 const App = () => {
 
-    const myArray = [
+    const [myArray, myCustomHandler] = useState([
         {id: "1", text: "1. Таджикская ССР – 5 651"},
         {id: "2", text: "2. Белорусская ССР – 4 933"},
         {id: "3", text: "3. Киргизская ССР – 3 975"},
@@ -13,12 +14,10 @@ const App = () => {
         {id: "5", text: "5. Молдавская ССР – 2 873"},
         {id: "6", text: "6. Эстонская ССР – 1 669"},
         {id: "7", text: "7. Литовская ССР – 1 655"}
-    ]
+    ]);
 
     const handleAddition = (newArrayElement) => {
-        myArray.push(newArrayElement)
-        console.log(newArrayElement)
-        console.log(myArray)
+        myCustomHandler(myArray.concat(newArrayElement));
     };
 
   return (
